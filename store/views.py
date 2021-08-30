3da1181e14f84c83ed647d916d2ad4782756bf8f
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-08-30 09:37:14 -0300

    Build Cart Items

--- store/views.py
+++ store/views.py
@@ -34,21 +34,37 @@ def cart(request):
             cart = json.loads(request.COOKIES['cart'])
         except:
             cart = {}
-            print('Cart:', cart)
-
+        print('Cart:', cart)
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
 
         for i in cart:
-            cartItems += cart[i]["quantity"]
+            try:
+                cartItems += cart[i]["quantity"]
 
-            product = Product.objects.get(id=i)
-            total = (product.price * cart[i]["quantity"])
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]["quantity"])
 
-            order['get_cart_total'] += total
-            order['get_cart_items'] += cart[i]["quantity"]
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]["quantity"]
 
+                item = {
+                    'product':{
+                        'id': product.id,
+                        'name': product.name,
+                        'price': product.price,
+                        'imageURL': product.imageURL,
+                    },
+                    'quantity':cart[i]['quantity'],
+                    'get_total': total
+                }
+                items.append(item)
+
+                if product.digital == False:
+                    order['shipping'] = True
+            except:
+                pass
     context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/cart.html', context)
 
